d9325461734a7c140222d2aa1cd5a2cb95c2dd05
kwucslkm <kwucsani@naver.com>
2023-03-13 22:33:57 +0900

    second

--- src/javagitex/Hello.java
+++ src/javagitex/Hello.java
@@ -5,6 +5,10 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		System.out.println("mysecond");
+		System.out.println("mysecond");
+		System.out.println("mysecond");
+		
 	}
 
 }
